164b04f9789d30f5c9262d738a704d146ba642b3
shinsw627 <shinsw627@naver.com>
2024-02-09 22:07:11 +0900

    ADD: 빈 부모 타입으로 모두 조회하기 - Object 타입

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -43,10 +43,19 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 모두 조회하기")
     void findAllBeanByParentType() {
-        Map<String, DiscountPolicy> beansOfTypes = ac.getBeansOfType(DiscountPolicy.class);
-        assertThat(beansOfTypes.size()).isEqualTo(2);
-        for (String key : beansOfTypes.keySet()) {
-            System.out.println("key = " + key + " value = " + beansOfTypes.get(key));
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object 타입")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
         }
     }
 
